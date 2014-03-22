51f279c6d399c6c94b0dde75bf80e51e2ea7c74a
Sergej Orlov <wladimirych@gmail.com>
2014-03-22 23:10:28 +0400

    extend L.Map with standard include method istead of prototype

--- ext-leaflet/Map.js
+++ ext-leaflet/Map.js
@@ -1,9 +1,9 @@
-L.Map.prototype.latLngBoundsToSizeInPixels = function(bounds, zoom) {
-    var p1 = this.project(bounds.getSouthWest(), zoom);
-    var p2 = this.project(bounds.getNorthEast(), zoom);
-    var pixel_width = p2.x - p1.x;
-    var pixel_height = p1.y - p2.y;
-    return L.point(pixel_width, pixel_height, true);
-}
-
-
+L.Map.include({
+    latLngBoundsToSizeInPixels: function(bounds, zoom) {
+        var p1 = this.project(bounds.getSouthWest(), zoom);
+        var p2 = this.project(bounds.getNorthEast(), zoom);
+        var pixel_width = p2.x - p1.x;
+        var pixel_height = p1.y - p2.y;
+        return L.point(pixel_width, pixel_height, true);
+    }
+});
