e2b66c0b404cc1089b4b091dd8a571a2dfdf77fc
Sergej Orlov <wladimirych@gmail.com>
2014-03-26 02:40:42 +0400

    tracks are added to grid and can be removed ot switched on/off

--- tracks-control.js
+++ tracks-control.js
@@ -1,3 +1,4 @@
+/* global L */
 "use strict";
 
 L.Util.parseGpx = function(txt){
@@ -20,7 +21,7 @@ L.Util.parseGpx = function(txt){
             segments.push(getSegmentPoints(segments_elements[i]));
         }
         return segments;
-    }
+    };
     
     txt = txt.replace(/<([^ >]+):([^ >]+)/g, '<$1_$2');
     var dom = (new DOMParser()).parseFromString(txt,"text/xml");
@@ -32,9 +33,9 @@ L.Util.parseGpx = function(txt){
 
 L.Util.parseOziPlt = function(txt) {
     var lines = txt.split('\n');
-    if (lines[0].indexOf('OziExplorer Track Point File') != 0) {
+    if (lines[0].indexOf('OziExplorer Track Point File') !== 0) {
         throw "Not an OZI track file";
-    };
+    }
     var segments = [];
     var current_segment = [];
     for (var i = 6; i < lines.length; i++) {
@@ -42,7 +43,7 @@ L.Util.parseOziPlt = function(txt) {
         var fields = line.split(',');
         var lat = parseFloat(fields[0]);
         var lon = parseFloat(fields[1]);
-        var is_start_of_segment = parseInt(fields[2]);
+        var is_start_of_segment = parseInt(fields[2], 10);
         if (isNaN(lat) || isNaN(lon) || isNaN(is_start_of_segment)) {
             break;
         }
@@ -65,12 +66,14 @@ L.Control.PrintPages.Tracks = L.Control.extend({
         'plt': L.Util.parseOziPlt
     },
 
+    colors: ['#f00', '#0f0', '#00f', '#f0f', '#0ff'],
+
     onAdd: function(map){
         this._map = map;
         this._tracks = [];
         var container = L.DomUtil.create('div', 'leaflet-control leaflet-printpages-dialog');
         container.innerHTML = '\
-            <table class="form" class="print-page-tracks-items">\
+            <table class="form">\
                 <tr>\
                     <td colspan="4">\
                         <a class="print-page-button print-page-open-file" title="Open file"></a>\
@@ -94,7 +97,7 @@ L.Control.PrintPages.Tracks = L.Control.extend({
         this.url_field = container.querySelector('input.print-page-url-field');
         L.DomEvent.on(download_button, 'click', this.onDownloadButtonPressed, this);
         
-        this.elements_container = container.querySelector('table.print-page-tracks-items')
+        this.elements_grid = container.querySelector('table.form');
         return container;
     },
     
@@ -107,16 +110,17 @@ L.Control.PrintPages.Tracks = L.Control.extend({
         var url = this.url_field.value.trim();
         if (url) {
             this.addTrackFromUrl(url);
-        };
+        }
         this.url_field.value = '';
     },
 
     _parseTrackFile: function(file) {
         var ext = file.name.split('.').pop().toLowerCase();
+        var track;
         if (ext in this.parsers) {
             var parser = this.parsers[ext];
             try {
-                var track = parser(file.data).tracks;
+                track = parser(file.data).tracks;
             } catch (e) {
                 alert('Could not load file: ' + e);
                 return null;
@@ -129,6 +133,11 @@ L.Control.PrintPages.Tracks = L.Control.extend({
         return file;
     },
 
+    _getNextColor: function() {
+        this._color_index = ((this._color_index | 0) + 1) % this.colors.length;
+        return this.colors[this._color_index]
+    },
+
     _createPolyline: function(segments, color){
         var track_lines = [];
         for (var i=0; i < segments.length; i++) {
@@ -136,19 +145,63 @@ L.Control.PrintPages.Tracks = L.Control.extend({
                 .map(function(p){return {x: p.lng, y: p.lat}});
             var segment_filtered = L.LineUtil.simplify(segment, 0.0002)
                 .map(function(p){return {lat: p.y, lng: p.x}});
-            track_lines.push(L.polyline(segments[i], {color: '#f00'}));
-            track_lines.push(L.polyline(segment_filtered));
-            console.log(segments[i].length);
-            console.log(segment_filtered.length);
+            //track_lines.push(L.polyline(segments[i], {color: '#f00'}));
+            track_lines.push(L.polyline(segment_filtered, {color: color}));
+            //console.log(segments[i].length);
+            //console.log(segment_filtered.length);
         }
         var trackfile_layer = L.featureGroup(track_lines);
         return trackfile_layer.addTo(this._map);
     },
 
-    // options: name -- url or filename, retrivable -- if can be downloaded
-    _addTrack: function(segments, options) {
-        var polyline = this._createPolyline(segments);
+    _createTrackGridItem: function(track_obj) {
+        var el = L.DomUtil.create('tr', '', this.elements_grid);
+        el.innerHTML = '\
+        <td><input type="checkbox" checked="checked"></td>\
+        <td><div class="track-color-selector"></div></td>\
+        <td><span class="track-name" title="' + (track_obj.url || track_obj.name) + '">' + track_obj.name + '</span></td>\
+        <td><a class="track-delete-button" title="Remove track">X</a></td>';
+        var checkbox = el.querySelector('input');
+        var delete_button = el.querySelector('a.track-delete-button');
+        var color_legend =  el.querySelector('.track-color-selector');
         
+        color_legend.style.backgroundColor = track_obj.color;
+        L.DomEvent.on(checkbox, 'click', function(e){
+            this.setTrackVisibility(track_obj, e.target.checked);
+        }, this);
+        L.DomEvent.on(delete_button, 'click', function(){
+            this.removeTrack(track_obj);
+        }, this);
+
+        return el;
+    },
+
+    setTrackVisibility: function(track_obj, visible) {
+        console.log(track_obj, visible);
+        var isVisible = this._map.hasLayer(track_obj.polyline);
+        if (Boolean(isVisible) !== Boolean(visible)) {
+            if (visible) {
+                this._map.addLayer(track_obj.polyline);
+            } else {
+                this._map.removeLayer(track_obj.polyline);
+            }
+        }
+    },
+
+    removeTrack: function(track_obj) {
+        this._map.removeLayer(track_obj.polyline);
+        this.elements_grid.removeChild(track_obj.grid_item);
+        var i = this._tracks.indexOf(track_obj);
+        this._tracks.splice(i, 1);
+    },
+
+    // options: name -- url or filename, retrivable -- if can be downloaded
+    createTrackObj: function(segments, options) {
+        var track_obj = L.extend({}, options);
+        track_obj.color = this._getNextColor();
+        track_obj.polyline = this._createPolyline(segments, track_obj.color);
+        track_obj.grid_item = this._createTrackGridItem(track_obj);
+        this._tracks.push(track_obj);
     },
 
     addTrackFromUrl: function(url) {
@@ -158,11 +211,11 @@ L.Control.PrintPages.Tracks = L.Control.extend({
         var _this = this;
         get(url)
             .then(function(xhr) {
-                return _this._parseTrackFile({data: xhr.responseText, name: url.split('/').pop()})
+                return _this._parseTrackFile({data: xhr.responseText, name: url.split('/').pop()});
             }).done(function(data)
             {
-                _this._addTrack(data.geodata, {name: data.name, retrivable: true});
-            })
+                _this.createTrackObj(data.geodata, {name: data.name, retrivable: true, url: url});
+            });
     },
 
     // file -- js file object as retrievd from file input`s property "files"'
@@ -170,7 +223,7 @@ L.Control.PrintPages.Tracks = L.Control.extend({
         readFile(file)
             .then(this._parseTrackFile.bind(this))
             .done(function(data){
-                this._addTrack(data.geodata, {name: data.name, retrivable: false});
+                this.createTrackObj(data.geodata, {name: data.name, retrivable: false});
             }.bind(this));
     },
 
@@ -180,3 +233,5 @@ L.Control.PrintPages.Tracks = L.Control.extend({
 //        console.log(this.createPolylinesFromFile(file.name, file.data));
 //    }
 });
+
+
